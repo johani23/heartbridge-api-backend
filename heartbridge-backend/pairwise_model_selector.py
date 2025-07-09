0e6af9814d6c3c60ec4783e0bb80c889d3d0d60c
johani23 <ahmad.a.johani@gmail.com>
2025-07-09 16:03:50 +0300

    Update pairwise_model_selector.py

--- heartbridge-backend/pairwise_model_selector.py
+++ heartbridge-backend/pairwise_model_selector.py
@@ -1,1 +1,9 @@
+def select_pairwise_model(cluster):
+    models = {
+        "The Silent Doubter": "model_silent",
+        "The Idealist": "model_ideal",
+        "The Burnt Survivor": "model_burnt",
+        "The Data-Lover": "model_data"
+    }
+    return models.get(cluster, "model_default")
 # Contents of pairwise_model_selector.py
