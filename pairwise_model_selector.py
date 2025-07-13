9612ef7a2f72088605de910c1a52afe16e9ca126
johani23 <ahmad.a.johani@gmail.com>
2025-07-13 11:41:09 +0300

    Create pairwise_model_selector.py

---
+++ pairwise_model_selector.py
@@ -0,0 +1,8 @@
+def select_pairwise_model(cluster):
+    models = {
+        "The Silent Doubter": "model_silent",
+        "The Idealist": "model_ideal",
+        "The Burnt Survivor": "model_burnt",
+        "The Data-Lover": "model_data"
+    }
+    return models.get(cluster, "model_default")
